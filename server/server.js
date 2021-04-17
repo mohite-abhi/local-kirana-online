0501a8ebb46b5eb6ada4d6f32fe49e152cd21439
mohite-abhi <b518004@iiit-bh.ac.in>
2021-04-17 20:04:14 +0530

    create store list and item list api

--- server/server.js
+++ server/server.js
@@ -1,21 +1,41 @@
+const mongoStore = require("./mongoStore.js")
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const app = express();
 const port = process.env.PORT || 5000;
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+
+
+//ex run on browser: http://localhost:5000/api/itemsFromStore?storeName=Dingdong Groceries
+app.get('/api/itemsFromStore', (req, res) => {
+  mongoStore.Store
+        .findOne({storeName:req.query.storeName})
+        .populate('itemID')
+        .exec(function (err,item) {
+        if (err) return handleError(err);
+        res.send(item.itemID)
+        });
+   
+
 });
 
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+
+
+//ex run on browser: http://localhost:5000/api/storesFromLocation?pin=752030
+app.get('/api/storesFromLocation', (req, res) => {
+  mongoStore.Location
+        .findOne({pin:req.query.pin})
+        .populate('storeID')
+        .exec(function (err,store) {
+        if (err) return handleError(err);
+        res.send(store.storeID);
+        });
+   
+
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
