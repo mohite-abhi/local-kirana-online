987483e11359e1ed5aa14ffc93fbb41aff83fefd
root <root@localhost.localdomain>
2021-04-20 14:34:18 +0530

    added routing in pages

---
+++ src/App3.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Link } from "react-router-dom";
+
+    function Page1() {
+
+        return (
+          <div>
+            <p>
+              This is the third page.
+              <br />
+              Don't Click on the button below.
+            </p>
+          </div>
+        );
+
+    }
+
+    export default Page1;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter , Route , Switch } from "react-router-dom";
+
+import page0 from "./App0";
+import App from './App';
+import page2 from "./App3";
 
 ReactDOM.render(
+  <BrowserRouter>
+  <switch>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Route exact path="/" component={page0}/>
+    <Route path="/shop" component = {App} />
+    <Route path="/item" component = {page2} />
+  </React.StrictMode>
+  </switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
